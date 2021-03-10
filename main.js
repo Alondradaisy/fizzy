2b86e3f98f25e495b4f8b288612c8eb0691867f1
Alondra Warren <daisy.codeimmersives.com>
2021-03-09 22:40:55 -0800

    practice if else statements with conditions

--- main.js
+++ main.js
@@ -2,7 +2,18 @@
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
 
-
+let i = 0;
+for (i = 0; i <= 20; i++) {
+  if (i % 3 === 0) {
+   console.log("Fizz"); //meaning if input number is divisible by 3 return Fizz
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  if (i % 5 === 0 && i % 3 === 0) {
+    console.log("FizzBuzz"); 
+  } else {
+    console.log(i); // if the inputted number is not divisible by either number, return the number itself
+  }
+}
 
 
 
